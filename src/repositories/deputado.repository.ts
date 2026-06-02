7f05a9f1572a1e892f65e6411cd26a90d105652d
LucasLevyOB <oliveiralucaslevy@gmail.com>
2026-06-01 23:19:38 -0300

    feat: adiciona foto

--- src/repositories/deputado.repository.ts
+++ src/repositories/deputado.repository.ts
@@ -5,7 +5,7 @@ export class DeputadoRepository {
         const skip = (page - 1) * limit;
         return await Deputado
             .find()
-            .select({ nome: 1, ufNascimento: 1, _id: 1 })
+            .select({ nome: 1, ufNascimento: 1, urlFoto: 1, _id: 1 })
             .where({ idLegislaturaFinal: 57 })
             .skip(skip)
             .limit(limit)
